3dbe088e5a84ea8a93be2cd45bc954c36cc737b6
tsukasa-forever <engineer.tsukasa@gmail.com>
2021-07-17 01:14:17 +0900

    Initial setting

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+const app: express.Express = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*")
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+})
+
+app.listen(3000, () => {
+  console.log("Start on port 3000.")
+})
